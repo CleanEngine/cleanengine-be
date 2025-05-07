fd1721ccb9af2b02f8e74eafebaa3ac6d2647b3d
109an <109an94@gmail.com>
2025-05-07 19:10:23 +0900

    feat: 스케줄러를 통한 API요청 1. 5초마다 요청 - 사유 : 트럼프 코인의 체결 강도 : 30초~5분에 1건 2. 중복 문제 해결 - 근거 : Sequential_Id는 timeStamp를 포함하며, 값이 순차적으로 높아지며, 중복은 없음 (200건 확인 결과) 3. queue에 저장 - 테스트용으로 작업 종료 후 메모리 전체 출력

---
+++ src/main/java/com/cleanengine/coin/realitybot/api/ApiScheduler.java
@@ -0,0 +1,52 @@
+package com.cleanengine.coin.realitybot.api;
+
+import com.cleanengine.coin.realitybot.dto.Ticks;
+import com.cleanengine.coin.realitybot.service.TickService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ApiScheduler implements DisposableBean {
+
+    private final BithumbAPIClient bithumbAPIClient;
+    private long lastMaxSequentialId = 1L;
+    private final Queue<Ticks> ticksQueue = new LinkedList<>();
+
+    @Scheduled(fixedRate = 5000)
+    public void MarketDataRequest(){
+        String rawJson = bithumbAPIClient.get();
+        List<Ticks> gson = TickService.paraseGson(rawJson);
+//        Queue<Ticks> gsonQueue = (Queue<Ticks>) TickService.paraseGson(rawJson);
+//        for(Ticks ticks : gson){
+        for (int i = gson.size()-1; i >=0 ; i--) {//10
+            Ticks ticks = gson.get(i);
+//            System.out.println("fori = " + gson.size());
+            if (ticks.getSequential_id() > lastMaxSequentialId){
+                System.out.println(ticks.getSequential_id());
+                ticksQueue.add(ticks);
+                lastMaxSequentialId = Math.max(lastMaxSequentialId, ticks.getSequential_id());
+                System.out.println("if = "+ticksQueue.size());
+            }
+        }
+        System.out.println(ticksQueue.size());
+    }
+    @Override
+    public void destroy() throws Exception {
+        log.info("종료 전 큐 데이터 출력");
+        ticksQueue.forEach(tick -> log.info(tick.toString()));
+        log.info("총 {}건의 데이터 출력 완료",ticksQueue.size());
+//        ticksQueue.clear();
+    }
+
+
+}

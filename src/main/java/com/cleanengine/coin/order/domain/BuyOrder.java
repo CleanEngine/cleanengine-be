3b7e9290c87f54dec00f4df696d5d053bc8cebd7
Junh-b <jooha208@gmail.com>
2025-05-07 14:52:44 +0900

    feat: 주문 핵심 Entity 초기 정의
    주문 핵심 로직에 필요한 BuyOrder, SellOrder, Asset 엔티티를 정의했습니다.

---
+++ src/main/java/com/cleanengine/coin/order/domain/BuyOrder.java
@@ -0,0 +1,59 @@
+package com.cleanengine.coin.order.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "buy_orders")
+@Table(name="buy_orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class BuyOrder {
+    @Id @Column(name="buy_id", nullable = false) @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="ticker", length = 10, nullable = false, updatable = false)
+    private String ticker;
+
+    @Column(name="user_id", nullable = false, updatable = false)
+    private Long userId;
+
+    // TODO size를 VO로 바꾸어야 함
+    @Column(name="size")
+    private Double size;
+
+    // TODO price를 VO로 바꾸어야 함
+    @Column(name="price", nullable = false)
+    private Double price;
+
+    @Column(name="created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name="is_marketorder", nullable = false, updatable = false)
+    private Boolean isMarketOrder;
+
+    @Column(name="is_bot", nullable = false, updatable = false)
+    private Boolean isBot;
+
+    public void buy(Double buySize){
+        if(buySize > this.size) throw new IllegalArgumentException();
+        this.size -= buySize;
+    }
+
+    public static BuyOrder create(String ticker, Long userId, Double size, Double price, LocalDateTime createdAt,
+                                  Boolean isMarketOrder, Boolean isBot) {
+        BuyOrder buyOrder = new BuyOrder();
+        buyOrder.ticker = ticker;
+        buyOrder.userId = userId;
+        buyOrder.size = size;
+        buyOrder.price = price;
+        buyOrder.createdAt = createdAt;
+        buyOrder.isMarketOrder = isMarketOrder;
+        buyOrder.isBot = isBot;
+        return buyOrder;
+    }
+}
---
+++ src/main/java/com/cleanengine/coin/order/domain/SellOrder.java
@@ -0,0 +1,58 @@
+package com.cleanengine.coin.order.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "sell_orders")
+@Table(name="sell_orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class SellOrder {
+    @Id @Column(name="buy_id", nullable = false) @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="ticker", length = 10, nullable = false, updatable = false)
+    private String ticker;
+
+    @Column(name="user_id", nullable = false, updatable = false)
+    private Long userId;
+
+    // TODO size를 VO로 바꾸어야 함
+    @Column(name="size", nullable = false)
+    private Double size;
+
+    // TODO price를 VO로 바꾸어야 함
+    @Column(name="price")
+    private Double price;
+
+    @Column(name="created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name="is_marketorder", nullable = false, updatable = false)
+    private Boolean isMarketOrder;
+
+    @Column(name="is_bot", nullable = false, updatable = false)
+    private Boolean isBot;
+
+    public void buy(Double buySize){
+        if(buySize > this.size) throw new IllegalArgumentException();
+        this.size -= buySize;
+    }
+
+    public static SellOrder create(String ticker, Long userId, Double size, Double price, LocalDateTime createdAt,
+                                   Boolean isMarketOrder, Boolean isBot) {
+        SellOrder sellOrder = new SellOrder();
+        sellOrder.ticker = ticker;
+        sellOrder.userId = userId;
+        sellOrder.size = size;
+        sellOrder.price = price;
+        sellOrder.createdAt = createdAt;
+        sellOrder.isMarketOrder = isMarketOrder;
+        sellOrder.isBot = isBot;
+        return sellOrder;
+    }
+}

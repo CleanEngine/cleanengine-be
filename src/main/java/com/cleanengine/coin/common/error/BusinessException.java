eb5a6000b883ad86502dee4c40636ee0fba0c09a
Junh-b <jooha208@gmail.com>
2025-05-11 16:32:38 +0900

    feat: DomainValidationException 추가
    서비스 외의 영역에서도 입력값 오류로 판단될 수 있는 유형의 상황이 일어난 경우, FieldError를 통해 일관된 응답을 반환할 수 있도록 DomainValidationException을 추가했습니다.
    BaseExceptionHandler에서도 이 Exception에 대한 처리 로직을 추가했습니다.

--- src/main/java/com/cleanengine/coin/common/error/BusinessException.java
+++ src/main/java/com/cleanengine/coin/common/error/BusinessException.java
@@ -1,7 +1,9 @@
 package com.cleanengine.coin.common.error;
 
 import com.cleanengine.coin.common.response.ErrorStatus;
+import lombok.Getter;
 
+@Getter
 public class BusinessException extends RuntimeException {
     private final ErrorStatus errorStatus;
 

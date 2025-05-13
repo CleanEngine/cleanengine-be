e27e939db32b3f0814f104ee54682f02dbe83ea2
bongj9 <lollypark501@naver.com>
2025-05-13 13:00:29 +0900

    refact: 불필요한 파일 삭제

--- src/main/java/com/cleanengine/coin/chart/service/ChartRealTimeService.java
+++
@@ -1,9 +0,0 @@
-package com.cleanengine.coin.chart.service;
-
-
-import com.cleanengine.coin.chart.Dto.RealTimeTradeDto;
-
-public interface ChartRealTimeService
-{
-    RealTimeTradeDto getRealTimeTrade(String ticker);
-}

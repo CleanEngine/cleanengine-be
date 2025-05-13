e1a45f59f6cf02d8d1b6f244aedaa9146d0cf73d
bongj9 <lollypark501@naver.com>
2025-05-13 14:21:04 +0900

    feat:테스트를 위해 0.3초 체결내역 생성 구현

---
+++ src/main/java/com/cleanengine/coin/trade/schedular/TradeDataGenerator.java
@@ -0,0 +1,22 @@
+package com.cleanengine.coin.trade.schedular;
+
+import com.cleanengine.coin.trade.service.TradeService;
+import com.cleanengine.coin.trade.service.TradeServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class TradeDataGenerator {
+
+    private final TradeServiceImpl tradeService;
+
+    @Scheduled(fixedDelay = 300) // 0.5초마다 실행
+    public void generateTradeData() {
+        tradeService.generateRandomTrade();
+        log.info("Random trade generated successfully.");
+    }
+}

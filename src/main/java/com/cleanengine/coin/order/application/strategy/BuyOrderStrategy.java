ee069384f59838c0f63857efb61628b9034c7448
Junh-b <jooha208@gmail.com>
2025-05-14 17:46:36 +0900

    feat: 첫 주문시 wallet 생성 기능 추가
    기본적으로 사용자가 회원가입시 wallet이 초기화되지 않지만, 사용자가 가상화폐별 첫 주문시도시 Wallet을 생성하는 로직을 추가했습니다. 이후 리팩토링 예정입니다.

--- src/main/java/com/cleanengine/coin/order/application/strategy/BuyOrderStrategy.java
+++ src/main/java/com/cleanengine/coin/order/application/strategy/BuyOrderStrategy.java
@@ -6,8 +6,12 @@ import com.cleanengine.coin.order.application.port.AccountUpdatePort;
 import com.cleanengine.coin.order.application.queue.OrderQueueManagerPool;
 import com.cleanengine.coin.order.domain.BuyOrder;
 import com.cleanengine.coin.order.domain.domainservice.CreateBuyOrderDomainService;
+import com.cleanengine.coin.order.external.adapter.account.AccountExternalRepository;
+import com.cleanengine.coin.order.external.adapter.wallet.WalletExternalRepository;
 import com.cleanengine.coin.order.infra.BuyOrderRepository;
 import com.cleanengine.coin.orderbook.application.service.UpdateOrderBookUsecase;
+import com.cleanengine.coin.user.domain.Account;
+import com.cleanengine.coin.user.domain.Wallet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +23,8 @@ public class BuyOrderStrategy extends CreateOrderStrategy<BuyOrder, OrderInfo<Bu
     private final OrderQueueManagerPool orderQueueManagerPool;
     private final AccountUpdatePort accountUpdatePort;
     private final UpdateOrderBookUsecase updateOrderBookUsecase;
+    private final WalletExternalRepository walletRepository;
+    private final AccountExternalRepository accountRepository;
 
     @Override
     public BuyOrder createOrder(OrderCommand.CreateOrder createOrderCommand) {
@@ -30,6 +36,15 @@ public class BuyOrderStrategy extends CreateOrderStrategy<BuyOrder, OrderInfo<Bu
     @Override
     public void saveOrder(BuyOrder order) {
         buyOrderRepository.save(order);
+    }
+
+    @Override
+    protected void createWallet(Integer userId, String ticker) {
+        if(walletRepository.findWalletBy(userId, ticker).isEmpty()){
+            Account account = accountRepository.findByUserId(userId).orElseThrow();
+            Wallet wallet = new Wallet(null, ticker, account.getId(), 0.0, 0.0, 0.0);
+            walletRepository.save(wallet);
+        }
     }
 
     @Override
--- src/main/java/com/cleanengine/coin/order/application/strategy/CreateOrderStrategy.java
+++ src/main/java/com/cleanengine/coin/order/application/strategy/CreateOrderStrategy.java
@@ -4,24 +4,26 @@ import com.cleanengine.coin.order.application.OrderCommand;
 import com.cleanengine.coin.order.application.OrderInfo;
 import com.cleanengine.coin.order.application.queue.OrderQueueManagerPool;
 import com.cleanengine.coin.order.domain.Order;
-import com.cleanengine.coin.orderbook.application.service.UpdateOrderBookUsecase;
 
 public abstract class CreateOrderStrategy<T extends Order, S extends OrderInfo<?>> {
 
     public S processCreatingOrder(OrderCommand.CreateOrder createOrderCommand){
         T order = createOrder(createOrderCommand);
         saveOrder(order);
+        createWallet(order.getUserId(), order.getTicker());
         keepHoldings(order);
         orderQueueManagerPool().addOrder(order.getTicker(), order);
         updateOrderBook(order);
         return extractOrderInfo(order);
     }
 
+    public abstract boolean supports(Boolean isBuyOrder);
+
     protected abstract T createOrder(OrderCommand.CreateOrder createOrderCommand);
     protected abstract void saveOrder(T order);
-    protected abstract S extractOrderInfo(T order);
-    public abstract boolean supports(Boolean isBuyOrder);
+    protected abstract void createWallet(Integer userId, String ticker);
     protected abstract void keepHoldings(T order) throws RuntimeException;
     protected abstract OrderQueueManagerPool orderQueueManagerPool();
     protected abstract void updateOrderBook(T order);
+    protected abstract S extractOrderInfo(T order);
 }

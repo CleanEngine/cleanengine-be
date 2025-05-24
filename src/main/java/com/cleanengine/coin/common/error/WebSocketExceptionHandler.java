c7a75260261719943db99874350ac3f311ac88f2
Junh-b <jooha208@gmail.com>
2025-05-25 01:57:09 +0900

    feat: Websocket ExceptionHandler 예외처리 추가
    기존 DomainException만 커버하던 Websocket ExceptionHandler가 범용적으로 Exception을 catch하여, handling되지 않는 에러일 경우 warn level의 로그를 작성토록 수정했습니다.

--- src/main/java/com/cleanengine/coin/common/error/WebSocketExceptionHandler.java
+++ src/main/java/com/cleanengine/coin/common/error/WebSocketExceptionHandler.java
@@ -1,15 +1,31 @@
 package com.cleanengine.coin.common.error;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
+@Slf4j
 public class WebSocketExceptionHandler {
 
     @MessageExceptionHandler(DomainValidationException.class)
     @SendToUser(value = "/queue/errors", broadcast = false)
     public String handleDomainValidationException(DomainValidationException e) {
         return e.getMessage();
+    }
+
+    @MessageExceptionHandler(Exception.class)
+    public void handleException(Exception e, @Payload String payload) {
+        StringBuilder logMessage = new StringBuilder();
+        logMessage.append("=======Websocket 요청 중 Handling 되지 않는 에러 발생=======");
+        logMessage.append("\n발생 시간 : ").append(LocalDateTime.now());
+        logMessage.append("\n요청 내용 : ").append(payload);
+        logMessage.append("\n예외 타입 : ").append(e.getClass().getName());
+        logMessage.append("\n메시지 : ").append(e.getMessage());
+        log.warn("{}", logMessage, e);
     }
 }

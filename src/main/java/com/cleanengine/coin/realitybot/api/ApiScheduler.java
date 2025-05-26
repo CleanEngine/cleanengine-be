2a681d17f6948d9bfeef16d8f4053df99c82f470
109an <109an94@gmail.com>
2025-05-26 18:04:40 +0900

    refactor: 초기 vwap 생성 이슈
    issue : #81

--- src/main/java/com/cleanengine/coin/realitybot/api/ApiScheduler.java
+++ src/main/java/com/cleanengine/coin/realitybot/api/ApiScheduler.java
@@ -52,33 +52,17 @@ public class ApiScheduler {
             Ticks ticks = gson.get(i);
             if (ticks.getSequential_id() > lastSeqId){ //중복 검증용
                 apiVWAPService.addTick(ticks);
-               /* //추세 갯수 지정 및 queue관리
-                if (ticksQueue.size()>=10){ //갯수를 10개 까지만으로 제한
-                    ticksQueue.poll();  //queue에서 빼기, 추세를 구현하기만 하면 필요가 있을까? -> 없음
-                }//sequentialId가 역으로 받아 poll을 먼저해야 add가능
-
-                //중복 검사 후 ticks 추가
-                ticksQueue.add(ticks);*/
                 lastSeqId = Math.max(lastSeqId, ticks.getSequential_id()); //중복 id 갱신
 
             }
         }
         lastSequentialIdMap.put(ticker,lastSeqId);
+        double vwap = apiVWAPService.getVWAP();
+        double volume = apiVWAPService.getAvgVolumePerOrder();
+        orderGenerateService.generateOrder(ticker,vwap,volume); //1tick 당 매수/매도 3개씩 제작
+//        log.info("작동확인 {}의 가격 : {} , 볼륨 : {}",ticker, vwap, volume);
+    }
 
-        //api 값으로 추세(VWAP)와 가상추세(VirtualVWAP) 구하기
-        if (apiVWAPService.getTickSize()>=10){ //10개 이전 작동시 order 에런 발생 (-300~300원 대량주문 / 호가 단위 때문에)
-            double vwap = apiVWAPService.getVWAP();
-            double volume = apiVWAPService.getAvgVolumePerOrder();
-//            tickService.processVWAP();//평균 체결 금액(VWAP) 구하기 (추세)
-
-            //생성 된 vwap으로 주문 로직 실행 TODO 비동기로 전환하기
-            orderGenerateService.generateOrder(ticker,vwap,volume); //1tick 당 매수/매도 3개씩 제작
-            log.info("작동확인 {}의 가격 : {} , 볼륨 : {}",ticker, vwap, volume);
-        } else {
-            log.info("작동불가 {}는 갯수가 {}",ticker,apiVWAPService.getTickSize());
-        };
-
-    };
 /*    @Override
     public void destroy() throws Exception { //담긴 Queue데이터 확인용
 //        log.info("종료 전 큐 데이터 출력");

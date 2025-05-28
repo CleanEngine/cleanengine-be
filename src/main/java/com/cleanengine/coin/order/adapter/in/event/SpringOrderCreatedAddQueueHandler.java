e34808297a00f99057a8074902c27a92d92fe3b6
Junh-b <jooha208@gmail.com>
2025-05-28 09:40:02 +0900

    fix: 주문이 큐에 삽입될 때 새 이벤트 발행
    체결측이 주문 삽입을 인지할 수 있도록 이벤트 발행을 추가했습니다.

--- src/main/java/com/cleanengine/coin/order/adapter/in/event/SpringOrderCreatedAddQueueHandler.java
+++ src/main/java/com/cleanengine/coin/order/adapter/in/event/SpringOrderCreatedAddQueueHandler.java
@@ -2,8 +2,10 @@ package com.cleanengine.coin.order.adapter.in.event;
 
 import com.cleanengine.coin.order.application.event.OrderCreated;
 import com.cleanengine.coin.order.adapter.out.persistentce.order.queue.OrderQueueManagerPool;
+import com.cleanengine.coin.order.application.event.OrderInsertedToQueue;
 import com.cleanengine.coin.order.domain.Order;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -11,10 +13,12 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @RequiredArgsConstructor
 public class SpringOrderCreatedAddQueueHandler {
     private final OrderQueueManagerPool orderQueueManagerPool;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     @TransactionalEventListener(OrderCreated.class)
     public void handleOrderCreated(OrderCreated event) {
         Order order = event.order();
         orderQueueManagerPool.addOrder(order.getTicker(), order);
+        applicationEventPublisher.publishEvent(new OrderInsertedToQueue(order));
     }
 }

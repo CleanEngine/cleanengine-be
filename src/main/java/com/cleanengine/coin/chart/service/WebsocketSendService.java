57b17d65c934ef771943428067b27905d2c7531a
bongj9 <lollypark501@naver.com>
2025-05-29 15:24:09 +0900

    refactor:전날 종가 메세지 양식 추가

--- src/main/java/com/cleanengine/coin/chart/service/WebsocketSendService.java
+++ src/main/java/com/cleanengine/coin/chart/service/WebsocketSendService.java
@@ -1,5 +1,6 @@
 package com.cleanengine.coin.chart.service;
 
+import com.cleanengine.coin.chart.dto.PrevRateDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -12,15 +13,21 @@ public class WebsocketSendService {
     private final SimpMessagingTemplate messagingTemplate;
 
 
-    //전송만해
-    public void send(Object data, String ticker){
+    //직전 데이터등락율 보내는 메세지 형식
+    public void sendChangeRate(Object data, String ticker) {
         log.debug("티커 {} 실시간 구독 요청", ticker);
 
         // 티커별 토픽으로 전송
         messagingTemplate.convertAndSend(
-                "/topic/realTimeTradeRate/" + ticker,
-                data
-        );
+                "/topic/realTimeTradeRate/" + ticker, data);
         log.debug("전송 완료: /topic/realTimeTradeRate/{} -> {}", ticker, data);
+    }
+
+    //전날 종가 변동률로 보내는 메세지 형식
+    public void sendPrevRate(Object data, String ticker) {
+        log.debug("티커 {} 전일 대비 변동률 보내는 요청", ticker);
+        messagingTemplate.convertAndSend(
+                "/topic/prevRate/" + ticker, data);
+        log.debug("전송 완료: /topic/prevRate/{} -> {}", ticker, data);
     }
 }

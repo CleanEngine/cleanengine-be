cf35215d011fdceb191563da84a4be70fcdac3f8
109an94 <109an94@gmail.com>
2025-06-08 00:58:05 +0900

    test: refresher 코드 수정

--- src/main/java/com/cleanengine/coin/realitybot/api/UnitPriceRefresher.java
+++ src/main/java/com/cleanengine/coin/realitybot/api/UnitPriceRefresher.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,7 +18,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
-@Service
+@Component
 @RequiredArgsConstructor
 public class UnitPriceRefresher implements ApplicationRunner {
     private final UnitPricePolicy unitPricePolicy;

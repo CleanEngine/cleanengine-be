38d2f8ae253d7de35ead55ca5d29007178a95f53
109an94 <109an94@gmail.com>
2025-06-11 22:52:46 +0900

    add: 주석 삭제

--- src/main/java/com/cleanengine/coin/realitybot/api/H2UnitPriceRefresher.java
+++ src/main/java/com/cleanengine/coin/realitybot/api/H2UnitPriceRefresher.java
@@ -1,21 +1,12 @@
 package com.cleanengine.coin.realitybot.api;
 
-import com.cleanengine.coin.order.adapter.out.persistentce.asset.AssetRepository;
-import com.cleanengine.coin.order.domain.Asset;
-import com.cleanengine.coin.realitybot.dto.OpeningPrice;
-import com.cleanengine.coin.realitybot.parser.OpeningPriceParser;
-import com.cleanengine.coin.realitybot.vo.UnitPricePolicy;
+
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @Component

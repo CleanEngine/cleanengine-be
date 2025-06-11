f0980de943ecbc0cff2b5dc9219ed393b9e35ec9
109an <109an94@gmail.com>
2025-06-11 13:00:56 +0900

    refactor: metric overhead 개선

--- src/main/java/com/cleanengine/coin/realitybot/domain/VWAPMetricsRecorder.java
+++ src/main/java/com/cleanengine/coin/realitybot/domain/VWAPMetricsRecorder.java
@@ -1,5 +1,6 @@
 package com.cleanengine.coin.realitybot.domain;
 
+import io.micrometer.core.instrument.DistributionSummary;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tags;
 import lombok.RequiredArgsConstructor;
@@ -14,20 +15,35 @@ import java.util.concurrent.atomic.AtomicReference;
 public class VWAPMetricsRecorder {
     private final MeterRegistry meterRegistry;
     private final ConcurrentHashMap<String, AtomicReference<Double>> orderPriceMap = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, DistributionSummary> orderPriceSummery = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<String, AtomicReference<Double>> apiVwapMap = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<String, AtomicReference<Double>> platformVwapMap = new ConcurrentHashMap<>();
 
     public void recordPrice(String ticker, boolean isBuy, double price){
         String type = isBuy ? "buy" : "sell";
         String timeStamp = Instant.now().toString();
-        String key = ticker +"|"+type+"|"+timeStamp;
+//        String key = ticker +"|"+type+"|"+timeStamp;
+        String key = ticker +"|"+type;
 
-        AtomicReference<Double> value = new AtomicReference<>(price);
 
-        meterRegistry.gauge("order_price",
-                Tags.of("ticker",ticker,"type",type,"timestamp",timeStamp)
-                ,value,AtomicReference::get);
-        orderPriceMap.put(key, value);
+        AtomicReference<Double> priceRef = orderPriceMap.computeIfAbsent(key, k -> {
+
+            AtomicReference<Double> value = new AtomicReference<>(price);
+
+            meterRegistry.gauge("order_price",
+                    Tags.of("ticker",ticker,"type",type)
+                    ,value,AtomicReference::get);
+            return value;
+        });
+        priceRef.set(price);
+
+        DistributionSummary summary = orderPriceSummery.computeIfAbsent(key,k ->
+                DistributionSummary.builder("order_price_summary")
+                        .tags(Tags.of("ticker",ticker,"type",type))
+                        .publishPercentiles(0.05,0.95)
+                        .register(meterRegistry)
+        );
+        summary.record(price);
     }
 
 

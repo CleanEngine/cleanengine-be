165a27a83d9e41523ec7f45459f94de06a1e3696
Junh-b <jooha208@gmail.com>
2025-06-16 12:45:28 +0900

    feat: ConcurrentSkipListSet기반 큐 구현
    기존 PriorityBlockingQueue 기반의 주문 큐를, ConcurrentSkipListMap, ConcurrentSkipListSet을 사용한 구조로 변경했습니다.

---
+++ src/main/java/com/cleanengine/coin/order/adapter/out/persistentce/order/queue/NestedInMemoryBuyOrderSkipListSet.java
@@ -0,0 +1,96 @@
+package com.cleanengine.coin.order.adapter.out.persistentce.order.queue;
+
+import com.cleanengine.coin.common.domain.port.PriorityQueueStore;
+import com.cleanengine.coin.order.domain.BuyOrder;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class NestedInMemoryBuyOrderSkipListSet implements PriorityQueueStore<BuyOrder> {
+    private final ConcurrentSkipListMap<Double, ConcurrentSkipListSet<BuyOrder>> map = new ConcurrentSkipListMap<>(Comparator.reverseOrder());
+    private final AtomicLong size = new AtomicLong();
+
+    @Override
+    public void put(BuyOrder item) {
+        if(item == null) throw new IllegalArgumentException("item cannot be null.");
+
+        map.compute(item.getPrice(), (key, buyOrders) -> {
+            if(buyOrders == null) {
+                buyOrders = new ConcurrentSkipListSet<>(Comparator.reverseOrder());
+            }
+            boolean added = buyOrders.add(item);
+            if(added) size.incrementAndGet();
+            return buyOrders;
+        });
+    }
+
+    @Override
+    public BuyOrder poll() {
+        while(true) {
+            Map.Entry<Double, ConcurrentSkipListSet<BuyOrder>> firstEntry = map.firstEntry();
+
+            if (firstEntry == null) {
+                return null;
+            }
+
+            ConcurrentSkipListSet<BuyOrder> buyOrders = firstEntry.getValue();
+            try {
+                BuyOrder order = buyOrders.first();
+                this.remove(order);
+                return order;
+            } catch (NoSuchElementException e) {
+                continue;
+            }
+        }
+    }
+
+    @Override
+    public BuyOrder peek() {
+        while(true) {
+            Map.Entry<Double, ConcurrentSkipListSet<BuyOrder>> firstEntry = map.firstEntry();
+
+            if (firstEntry == null) {
+                return null;
+            }
+
+            ConcurrentSkipListSet<BuyOrder> buyOrders = firstEntry.getValue();
+            try {
+                return buyOrders.first();
+            } catch (NoSuchElementException e) {
+                continue;
+            }
+        }
+    }
+
+    @Override
+    public BuyOrder remove(BuyOrder item) {
+        if (item == null) return null;
+
+        map.computeIfPresent(item.getPrice(), (key, orders) -> {
+            boolean removed = orders.remove(item);
+            if (removed) size.decrementAndGet();
+            return orders.isEmpty() ? null : orders;
+        });
+
+        return item;
+    }
+
+    @Override
+    public long size() {
+        return size.get();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+}

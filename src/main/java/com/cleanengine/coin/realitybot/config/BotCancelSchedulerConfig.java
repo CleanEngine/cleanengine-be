64a1164745efb8d8a3279697267c56c121577bee
Junh-b <jooha208@gmail.com>
2025-07-11 13:43:04 +0900

    feat: 봇 주문 취소 Scheduler 구현
    application yml의 설정 내용을 토대로, 미체결된 봇 주문들의 일부를 지속적으로 취소시키는 Scheduler를 추가했습니다. 기본 간격은 5초이며, 미체결 주문중 20%가 취소됩니다.

---
+++ src/main/java/com/cleanengine/coin/realitybot/config/BotCancelSchedulerConfig.java
@@ -0,0 +1,38 @@
+package com.cleanengine.coin.realitybot.config;
+
+import com.cleanengine.coin.common.annotation.WorkingServerProfile;
+import com.cleanengine.coin.realitybot.service.BotOrderCancelService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.time.Duration;
+
+@Configuration
+@RequiredArgsConstructor
+@WorkingServerProfile
+@Slf4j
+public class BotCancelSchedulerConfig implements SchedulingConfigurer {
+
+    private final BotOrderCancelService botOrderCancelService;
+
+    @Value("${bot-cancel-scheduler.fixed-rate}")
+    protected Duration fixedRate;
+
+    @Value("${bot-cancel-scheduler.cancel-rate}")
+    protected double cancelRate;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar registrar) {
+        registrar.addFixedRateTask(() -> {
+            try {
+                botOrderCancelService.cancelBotOrdersAllTicker(cancelRate);
+            } catch (Exception e) {
+                log.error("handling되지 않은 에러가 bot 주문취소 스케줄러에서 발생", e);
+            }
+        }, fixedRate);
+    }
+}

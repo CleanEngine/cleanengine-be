c46b702d64404a6d06f33fb6e50a084ed13b34db
109an94 <109an94@gmail.com>
2025-07-22 02:27:23 +0900

    remove: 주석 제거

--- src/main/java/com/cleanengine/coin/mypage/repository/CompletedBuyOrderRepository.java
+++ src/main/java/com/cleanengine/coin/mypage/repository/CompletedBuyOrderRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CompletedBuyOrderRepository extends JpaRepository<BuyOrder, Long> {
     Page<BuyOrder> findByUserId(Integer userId, Pageable pageable);
     long countByUserId(Integer userId);
--- src/main/java/com/cleanengine/coin/mypage/repository/CompletedSellOrderRepository.java
+++ src/main/java/com/cleanengine/coin/mypage/repository/CompletedSellOrderRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface CompletedSellOrderRepository extends JpaRepository<SellOrder, Integer> {
     Page<SellOrder> findByUserId(Integer userId, Pageable pageable);

8acf38fbf2adb02866702a8c31230497d917a0c6
109an94 <109an94@gmail.com>
2025-07-26 23:40:16 +0900

    rename: 체결 목록 조회 명칭 변경

--- src/main/java/com/cleanengine/coin/mypage/service/OrderListService.java
+++ src/main/java/com/cleanengine/coin/mypage/service/OrderListService.java
@@ -1,9 +1,9 @@
 package com.cleanengine.coin.mypage.service;
 
-import com.cleanengine.coin.mypage.dto.CompletedOrderDto;
-import com.cleanengine.coin.mypage.dto.PagedCompletedOrdersDto;
-import com.cleanengine.coin.mypage.repository.CompletedBuyOrderRepository;
-import com.cleanengine.coin.mypage.repository.CompletedSellOrderRepository;
+import com.cleanengine.coin.mypage.dto.OrderListDto;
+import com.cleanengine.coin.mypage.dto.PagedOrderListDto;
+import com.cleanengine.coin.mypage.repository.BuyOrderListRepository;
+import com.cleanengine.coin.mypage.repository.SellOrderListRepository;
 import com.cleanengine.coin.order.OrderSide;
 import com.cleanengine.coin.order.adapter.out.persistentce.asset.AssetRepository;
 import com.cleanengine.coin.order.domain.BuyOrder;
@@ -21,12 +21,12 @@ import java.util.stream.Stream;
 
 @Service
 @AllArgsConstructor
-public class CompletedOrderService {
-    private final CompletedBuyOrderRepository completedBuyOrderRepository;
-    private final CompletedSellOrderRepository completedSellOrderRepository;
+public class OrderListService {
+    private final BuyOrderListRepository buyOrderListRepository;
+    private final SellOrderListRepository sellOrderListRepository;
     private final AssetRepository assetRepository;
 
-    public PagedCompletedOrdersDto getCompletedOrders(Integer userId, int currentPage, int pageSize, boolean settled) {//todo cursortradetime, long cursorid, int pageisze 받기
+    public PagedOrderListDto getOrderList(Integer userId, int currentPage, int pageSize, boolean settled) {//todo cursortradetime, long cursorid, int pageisze 받기
         //커서기반에서 오프셋 기반으로 변경
         PageRequest pageRequest = PageRequest.of(currentPage, pageSize, Sort.by(Sort.Direction.DESC,"createdAt","id"));
         List<BuyOrder> buyOrders;
@@ -34,42 +34,42 @@ public class CompletedOrderService {
         long totalBuyOrders;
         long totalSellOrders;
         if (settled) {
-            buyOrders = completedBuyOrderRepository.findByUserIdAndState(userId, OrderStatus.DONE,pageRequest).getContent();
-            sellOrders = completedSellOrderRepository.findByUserIdAndState(userId,OrderStatus.DONE,pageRequest).getContent();
-            totalBuyOrders = completedBuyOrderRepository.countByUserIdAndState(userId,OrderStatus.DONE);
-            totalSellOrders = completedSellOrderRepository.countByUserIdAndState(userId,OrderStatus.DONE);
+            buyOrders = buyOrderListRepository.findByUserIdAndState(userId, OrderStatus.DONE,pageRequest).getContent();
+            sellOrders = sellOrderListRepository.findByUserIdAndState(userId,OrderStatus.DONE,pageRequest).getContent();
+            totalBuyOrders = buyOrderListRepository.countByUserIdAndState(userId,OrderStatus.DONE);
+            totalSellOrders = sellOrderListRepository.countByUserIdAndState(userId,OrderStatus.DONE);
         }else {
-            buyOrders = completedBuyOrderRepository.findByUserId(userId,pageRequest).getContent();
-            sellOrders = completedSellOrderRepository.findByUserId(userId,pageRequest).getContent();
-            totalBuyOrders = completedBuyOrderRepository.countByUserId(userId);
-            totalSellOrders = completedSellOrderRepository.countByUserId(userId);
+            buyOrders = buyOrderListRepository.findByUserId(userId,pageRequest).getContent();
+            sellOrders = sellOrderListRepository.findByUserId(userId,pageRequest).getContent();
+            totalBuyOrders = buyOrderListRepository.countByUserId(userId);
+            totalSellOrders = sellOrderListRepository.countByUserId(userId);
 
         }
-        List<CompletedOrderDto> buyDtos = buyOrders.stream()
+        List<OrderListDto> buyDtos = buyOrders.stream()
                 .map(b ->
-                        new CompletedOrderDto(OrderSide.BID,b.getState(),
+                        new OrderListDto(OrderSide.BID,b.getState(),
                                 convertToOrderType(b.getIsMarketOrder()),
                                 b.getId(), b.getTicker(),
                         assetRepository.findByTicker(b.getTicker()).getName(),b.getPrice()
                                 ,b.getOrderSize(),b.getRemainingSize(),
                                 calcDisplaySize(b.getState(),b.getOrderSize(),b.getRemainingSize())
                                 ,b.getCreatedAt())).toList();
-        List<CompletedOrderDto> sellDtos = sellOrders.stream()
+        List<OrderListDto> sellDtos = sellOrders.stream()
                 .map(s ->
-                        new CompletedOrderDto(OrderSide.ASK,s.getState(),
+                        new OrderListDto(OrderSide.ASK,s.getState(),
                                 convertToOrderType(s.getIsMarketOrder()),
                                 s.getId(), s.getTicker(),
                                 assetRepository.findByTicker(s.getTicker()).getName(),s.getPrice()
                                 ,s.getOrderSize(),s.getRemainingSize(),
                                 calcDisplaySize(s.getState(),s.getOrderSize(),s.getRemainingSize())
                                 ,s.getCreatedAt())).toList();
-        List<CompletedOrderDto> completedOrderDtos = Stream.concat(buyDtos.stream(),sellDtos.stream())
-                .sorted(Comparator.comparing(CompletedOrderDto::getTradeTime).reversed()).toList();
+        List<OrderListDto> completedOrderDtos = Stream.concat(buyDtos.stream(),sellDtos.stream())
+                .sorted(Comparator.comparing(OrderListDto::getTradeTime).reversed()).toList();
         long totalElements = totalBuyOrders + totalSellOrders;
 
         int totalPages = (int) Math.ceil((double)totalElements / pageSize);
 
-        return new PagedCompletedOrdersDto(totalPages,totalElements,currentPage,pageSize,completedOrderDtos);
+        return new PagedOrderListDto(totalPages,totalElements,currentPage,pageSize,completedOrderDtos);
     }
         private double calcDisplaySize(OrderStatus orderStatus,Double orderSize,Double remainingSize) {
             if (orderStatus == null) return 0.0;
